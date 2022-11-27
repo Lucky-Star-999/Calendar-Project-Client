b6e94b2fed325dae604c8ad3a0aa749d4699421a
Lucky-Star-999 <theboost1305@gmail.com>
2022-11-27 22:35:19 +0700

    Format Error Login Form

--- src/user/Login.js
+++ src/user/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Form, Input, Space } from 'antd';
+import { Button, Form, Input, Space, Modal } from 'antd';
 
 import { useState } from 'react';
 import axios from 'axios';
@@ -8,7 +8,7 @@ import background from "./bg.svg";
 import LoginLogo from "./LoginLogo.js";
 
 const Login = () => {
-  
+
 
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
@@ -20,6 +20,23 @@ const Login = () => {
 
   localStorage.clear();
 
+  const [open, setOpen] = useState(false);
+  const [open1, setOpen1] = useState(false);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+  const hideModal = () => {
+    setOpen(false);
+  };
+
+  const showModal1 = () => {
+    setOpen1(true);
+  };
+  const hideModal1 = () => {
+    setOpen1(false);
+  };
+
   const handleSubmit = (event) => {
 
     axios.post(`http://localhost:9000/user/login`, { email, password })
@@ -29,6 +46,10 @@ const Login = () => {
 
         if (res.data === 'Login successfully') {
           navigate("/home", { state: { email: email } });
+        } else if (res.data === 'Wrong password') {
+          showModal();
+        } else if (res.data === 'Username does not exist') {
+          showModal1();
         }
       });
   }
@@ -39,6 +60,28 @@ const Login = () => {
       backgroundImage: `url("${background}")`, minHeight: "100vh",
       display: "flex", justifyContent: "center", alignItems: "center"
     }}>
+
+      <Modal
+        title="Wrong password!"
+        open={open}
+        onOk={hideModal}
+        okText="Okay!"
+        onCancel={hideModal}
+        cancelText="Cancel"
+      >
+        <p>Your password is wrong! Please type carefully!</p>
+      </Modal>
+
+      <Modal
+        title="Email does not exist!"
+        open={open1}
+        onOk={hideModal1}
+        okText="Okay!"
+        onCancel={hideModal1}
+        cancelText="Cancel"
+      >
+        <p>The email does not exist! Please type carefully or create new account!</p>
+      </Modal>
 
       <Form autoComplete="off" onFinish={handleSubmit}
         style={{ minHeight: "40vh", minWidth: "30vw" }}>
